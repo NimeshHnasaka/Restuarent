8069ce28a51f6b2a04a166133e2a491bc66b3401
Your Name <you@example.com>
2024-02-24 11:33:26 +0530

    new

--- Self-Service-Resturent-main/backend/server.js
+++ Self-Service-Resturent-main/backend/server.js
@@ -150,4 +150,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
--- Self-Service-Resturent-main/frontend/src/Components/CaptureImage/CaptureImage.js
+++ Self-Service-Resturent-main/frontend/src/Components/CaptureImage/CaptureImage.js
@@ -56,91 +56,6 @@
 
 
 
-// import React, { useRef, useState } from 'react';
-// import Webcam from 'react-webcam';
-// import './CaptureImage.css'; // Import the CSS file for styling
-// import ObjectDetection from '../ObjectDetection/ObjectDetection';
-
-// const CaptureImage = () => {
-//     const webcamRef = useRef(null);
-//     const [loading, setLoading] = useState(false);
-//     const [showObjectDetection, setShowObjectDetection] = useState(false);
-//     const [capturedImage, setCapturedImage] = useState(null);
-   
-
-//     const capture = async () => {
-//         setLoading(true);
-//         const imageSrc = webcamRef.current.getScreenshot();
-//         //const imageSrc = '/f3.jpg';
-//         setCapturedImage(imageSrc);
-//         const formData = new FormData();
-//         formData.append('image', dataURItoBlob(imageSrc)); // Convert data URI to blob
-//         try {
-//             const response = await fetch('http://localhost:5000/api/upload', {
-//                 method: 'POST',
-//                 body: formData
-//             });
-//             if (response.ok) {
-//                 const responseData = await response.json();
-          
-//                 console.log(responseData)
-                
-//                 setLoading(false);
-//                 setShowObjectDetection(true); // Show the second screen
-//             } else {
-//                 throw new Error('Failed to upload image');
-//             }
-//         } catch (error) {
-//             console.error('Error uploading image:', error);
-//             setLoading(false);
-//         }
-//     };
-
-//     // Convert data URI to blob
-//     const dataURItoBlob = (dataURI) => {
-//         const byteString = atob(dataURI.split(',')[1]);
-//         const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-//         const ab = new ArrayBuffer(byteString.length);
-//         const ia = new Uint8Array(ab);
-//         for (let i = 0; i < byteString.length; i++) {
-//             ia[i] = byteString.charCodeAt(i);
-//         }
-//         return new Blob([ab], { type: mimeString });
-//     };
-
-//     return (
-//         <div className="capture-image-container">
-//             {showObjectDetection ? (
-//                 <ObjectDetection   capturedImage={capturedImage}
-//                />
-               
-//             ) : (
-//                 <div className="capture-form">
-//                     <h1 className="welcome-message">Welcome!</h1>
-//                     <h2 className="header">Webcam Image Capture</h2>
-//                     <h3 className="instruction">Put Your Tray Here</h3>
-//                     <div className="webcam-container">
-//                         <Webcam
-//                             audio={false}
-//                             ref={webcamRef}
-//                             screenshotFormat="image/jpeg"
-//                             className="webcam"
-//                         />
-//                     </div>
-//                     {loading ? (
-//                         <p className="loading-text">Capturing image...</p>
-//                     ) : (
-//                         <button className="capture-button" onClick={capture}>Capture Image</button>
-//                     )}
-//                 </div>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default CaptureImage;
-
-
 import React, { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import './CaptureImage.css'; // Import the CSS file for styling
@@ -151,6 +66,7 @@ const CaptureImage = () => {
     const [loading, setLoading] = useState(false);
     const [showObjectDetection, setShowObjectDetection] = useState(false);
     const [capturedImage, setCapturedImage] = useState(null);
+   
 
     const capture = async () => {
         setLoading(true);
@@ -166,7 +82,9 @@ const CaptureImage = () => {
             });
             if (response.ok) {
                 const responseData = await response.json();
-                console.log(responseData);
+          
+                console.log(responseData)
+                
                 setLoading(false);
                 setShowObjectDetection(true); // Show the second screen
             } else {
@@ -193,7 +111,9 @@ const CaptureImage = () => {
     return (
         <div className="capture-image-container">
             {showObjectDetection ? (
-                <ObjectDetection capturedImage={capturedImage} />
+                <ObjectDetection   capturedImage={capturedImage}
+               />
+               
             ) : (
                 <div className="capture-form">
                     <h1 className="welcome-message">Welcome!</h1>
@@ -219,3 +139,4 @@ const CaptureImage = () => {
 };
 
 export default CaptureImage;
+
